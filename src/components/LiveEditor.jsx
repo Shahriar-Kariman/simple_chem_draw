180c091e2b0b3693d1817f2e56d17520793f55cb
Shahriar Kariman <shahriar.kariman@gmail.com>
2024-11-07 15:20:03 -0400

    I changed the style of the text field.

--- src/components/LiveEditor.jsx
+++ src/components/LiveEditor.jsx
@@ -1,5 +1,44 @@
-import { TextField } from '@mui/material';
+import { TextField, ThemeProvider, createTheme } from '@mui/material'
 import React, { useState, useEffect } from 'react'
+
+const theme = createTheme({
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          // Outlined
+          "& .MuiOutlinedInput-root": {
+            color: "#000",
+            fontFamily: "Arial",
+            fontWeight: "bold",
+            "& .MuiOutlinedInput-notchedOutline": {
+              borderColor: "#6495ED",
+              borderWidth: "2px",
+            },
+            "&.Mui-focused": {
+              "& .MuiOutlinedInput-notchedOutline": {
+                borderColor: "#008080",
+                borderWidth: "3px",
+              },
+            },
+            "&:hover:not(.Mui-focused)": {
+              "& .MuiOutlinedInput-notchedOutline": {
+                borderColor: "#008080",
+              },
+            },
+          },
+          "& .MuiInputLabel-outlined": {
+            color: "#FFAA33",
+            fontWeight: "bold",
+            "&.Mui-focused": {
+              color: "#008080",
+            },
+          },
+        },
+      },
+    },
+  },
+});
 
 function LiveEditor({ height, setMol }) {
 
@@ -12,44 +51,48 @@ function LiveEditor({ height, setMol }) {
         padding: '12px'
       }
     }>
-      <TextField 
-        variant='outlined'
-        placeholder='CN1C=NC2=C1C(=O)N(C(=O)N2C)C'
-        label='SMILES Notations'
-        multiline
-        fullWidth
-        inputProps={{ style: { color: "red" } }}
-        onChange={
-          (e)=>{
-            setMol(e.target.value)
+      <ThemeProvider theme={theme} >
+        <TextField 
+          variant='outlined'
+          placeholder='CN1C=NC2=C1C(=O)N(C(=O)N2C)C'
+          label='SMILES Notations'
+          multiline
+          fullWidth
+          inputProps={{ 
+            style: { color: "#FFAA33" },
+          }}
+          onChange={
+            (e)=>{
+              setMol(e.target.value)
+            }
           }
-        }
-        sx={{
-          "& .MuiOutlinedInput-root": {
-            color: "#000",
-            fontFamily: "Arial",
-            fontWeight: "bold",
-            "& .MuiOutlinedInput-notchedOutline": {
-              borderColor: "#2e2e2e",
-              borderWidth: "2px",
-            },
-            "&.Mui-focused": {
-              "& .MuiOutlinedInput-notchedOutline": {
-                borderColor: "secondary.main",
-                borderWidth: "3px",
-              },
-            },
-            "& .MuiInputLabel-outlined": {
-              color: "#2e2e2e",
-              fontWeight: "bold",
-              "&.Mui-focused": {
-                color: "secondary.main",
-                fontWeight: "bold",
-              },
-            },
-          },
-        }}
-      />
+          // sx={{
+          //   "& .MuiOutlinedInput-root": {
+          //     color: "#000",
+          //     fontFamily: "Arial",
+          //     fontWeight: "bold",
+          //     "& .MuiOutlinedInput-notchedOutline": {
+          //       borderColor: "#008080",
+          //       borderWidth: "2px",
+          //     },
+          //     "&.Mui-focused": {
+          //       "& .MuiOutlinedInput-notchedOutline": {
+          //         borderColor: "#008080",
+          //         borderWidth: "3px",
+          //       },
+          //     },
+          //     "& .MuiInputLabel-outlined": {
+          //       color: "#008080",
+          //       fontWeight: "bold",
+          //       "&.Mui-focused": {
+          //         color: "secondary.main",
+          //         fontWeight: "bold",
+          //       },
+          //     },
+          //   },
+          // }}
+        />
+      </ThemeProvider>
     </div>
   )
 }
